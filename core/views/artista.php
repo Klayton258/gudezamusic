1b98d6d7a8328322d84dcc3fd802105a872bdd42
Klayton258 <klayton0304massango@gmail.com>
2021-11-23 17:06:23 +0200

    Atualizacao da Biografia

--- core/views/artista.php
+++ core/views/artista.php
@@ -35,29 +35,15 @@
             </p>
             <div class="collapse col-lg-6 col-sm-12" id="collapseExample">
                 <div class="card card-body text-center bio-content">
-                    <p>Carlos Chatuir Júnior, mais conhecido como <strong>Charles Caló</strong>, é um cantor, compositor
-                        moçambicano. Caló nasceu em Nampula, Moçambique no dia 26 de Abril de 2000.
+                    <p>Carlos Chatuir Júnior, mais conhecido como <strong>Charles Caló</strong>, é um cantor-compositor moçambicano. Caló nasceu em Nampula, Moçambique no dia 26 de Abril de 2000.
                     </p>
-                    <P> Lançou a sua primeira música oficial do “<strong>Curtir A Vibe</strong>” com os seus 14 anos de
-                        idade, música
-                        esta que abriu-lhe
-                        várias portas na indústria da música e lhe fez ganhar uma certa popularidade no mercado
-                        nacional.
+                    <P>Iniciou a sua carreira musical aos 14 anos de idade, enquanto ainda estava frequentar o ensino secundário. Lançou a sua primeira música oficial do “<strong>Curtir A Vibe</strong>” música esta que abriu-lhe várias portas na indústria da música e lhe fez ganhar uma certa popularidade no mercado nacional. Alguns anos depois, ele fundou sua própria gravadora independente “<strong>Gudeza Music</strong>” com o rapper Alsina Star, onde lançou três EP’s em anos seguidas na mesma: “<strong>Sentimentos Não Tem Idade</strong>” em 2017, “<strong>Quem Ama Sofre</strong>” em 2018 e “<strong>O Encantador Delas</strong>” em 2019.
                     </P>
-                    <p> Alguns anos depois, ele fundou sua própria gravadora independente “Gudeza Music” com o rapper
-                        Alsina Star, onde lançou três EP’s em anos seguidas na mesma: “<strong>Sentimentos Não Tem
-                            Idade</strong>” em 2017, “<strong>Quem Ama Sofre</strong>” em 2018 e “<strong>O Encantador
-                            Delas</strong>” em 2019. Caló atuou no evento Lizha Só Festas em 2016.
+                    <p> Caló teve a honra de atuar em 3 edições de um dos maiores eventos de Moçambique “Lizha Só Festas”. Em 2019 lança o seu 1º single do álbum “<strong>Miúda Mimada</strong>” com a participação do Rapper Youngg Ricardo nas plataformas digitais com a distribuição da Modigi, o single ocupou a 5ª posição nas paradas do ITunes MZ categoria R&B/Soul. 
                     </P>
-                    <P>Em 2019 lança o seu 1º single do álbum “Miúda Mimada” com a participação do
-                        Rapper Youngg Ricardo nas plataformas digitais com a distribuição da Modigi, o single ocupou a
-                        5ª posição nas paradas do ITunes MZ categoria R&B/Soul. Seu primeiro álbum de estúdio
-                        <a href="?a=novos_desafios">“Novos Desafios”</a> foi lançado no dia 26 de Junho de 2020,
-                        dois dias depois, o álbum alcançou a 28ª posição paradas do ITunes MZ. A música “Para Sempre”
-                        foi lançada como 2° single do álbum nas rádios e ocupou 1a posição
-                        no Top + da Rádio Viva e, o vídeo foi lançado no dia 14 de Maio no seu canal do YouTube.
+                    <P>Em 2020, após ter largado o ensino superior, lançou seu primeiro álbum de estúdio “<strong>Novos Desafios</strong>” foi lançado no dia 26 de Junho, dois dias depois, o álbum alcançou a 28ª posição nas paradas do ITunes MZ. 
                     </P>
-
+                    <p>A música “<strong>Para Sempre</strong>” foi lançada como 2° single do álbum nas rádios e ocupou 1a posição no Top + da Rádio Viva e, o vídeo foi lançado no dia 14 de Maio de 2021 no seu canal do YouTube. No dia 06 de Agosto de 2021, lançou a EP “<strong>Homem Mau</strong>” nas plataformas digitais, para promover a EP lançou o videoclipe da música “<strong>Vai Com Calma</strong>”.</p>
                 </div>
             </div>
         </div>
