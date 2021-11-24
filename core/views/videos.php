08f7f0b3aa6b3e67d9ff5d8009769130f3fd6260
Klayton258 <klayton0304massango@gmail.com>
2021-11-24 07:11:46 +0200

    Reverse da pagina de videos

--- core/views/videos.php
+++ core/views/videos.php
@@ -1,8 +1,8 @@
-<div class="container mb-5">
+<div class="container mb-2">
     <img src="assets/components/videos-in.png" class="titulo-art">
 </div>
 
-<style>
+<!-- <style>
 .gal-cont {
     display: flex;
     width: 98vw;
@@ -58,4 +58,34 @@
     </div>
 </div>
 
-<script src="js/lightbox-plus-jquery.js"></script>
+<script src="js/lightbox-plus-jquery.js"></script> -->
+
+<div class="container">
+    <div class="ratio ratio-16x9">
+        <iframe src="https://www.youtube.com/embed/sM_V0_7_rNw?rel=0&autoplay=1&showinfo=0" title="YouTube video"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; rel; picture-in-picture; showinfo=0"></iframe>
+    </div>
+</div>
+<div class="container mt-5">
+    <div class="ratio ratio-16x9">
+        <iframe src="https://www.youtube.com/embed/PUmAYF0agvQ?rel=0&showinfo=0" title="YouTube video"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; rel; picture-in-picture; showinfo=0"></iframe>
+    </div>
+</div>
+
+<div class="container mt-5">
+    <div class="ratio ratio-16x9">
+        <iframe src="https://www.youtube.com/embed/eCwMVfOxb_U?modestbranding=1&rel=0" title="YouTube video"
+            allow="accelerometer; clipboard-write; encrypted-media; modestbranding; gyroscope; picture-in-picture"></iframe>
+    </div>
+</div>
+
+<div class="container mt-5">
+    <div class="ratio mt-5 ratio-16x9">
+        <iframe src="https://www.youtube.com/embed/AwC0VnlC32g?modestbranding=1&rel=0" title="YouTube video"
+            allowfullscreen
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; modestbranding; gyroscope; picture-in-picture"
+            allowfullscreen></iframe>
+    </div>
+    <hr>
+</div>
