f5d0c8eaef749271511d067f158b8b98e5c0e255
Klayton Massango <kmassango@isutc.ac.mz>
2022-08-23 13:20:04 +0200

    Update Gudeza Music Logo

--- resources/views/layout/layout_main.blade.php
+++ resources/views/layout/layout_main.blade.php
@@ -7,13 +7,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="shortcut icon" href={{ asset('favicon.ico') }} type="image/x-icon">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{asset('assets/components/favicon/apple-touch-icon.png')}}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{asset('assets/components/favicon/favicon-32x32.png')}}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('assets/components/favicon/favicon-16x16.png')}}">
+    <link rel="shortcut icon" href="{{asset('assets/components/favicon/favicon.ico')}}" type="image/x-icon">
+    <link rel="manifest" href="{{asset('assets/components/favicon/site.webmanifest')}}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href={{asset('css/app.css')}}>
     <link rel="stylesheet" href={{asset('css/styles.css')}}>
     <link rel="stylesheet" href={{asset('css/main.css')}}>
-    <link rel="shortcut icon" href="{{asset('assets/components/favicon.ico')}}" type="image/x-icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>{{env('APP_NAME')}}</title>
 </head>
