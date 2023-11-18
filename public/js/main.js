509395841bff5b0004fbb3f0e00e1589ea698a91
Klayton258 <kmassango1@gmail.com>
2023-11-18 11:16:26 +0200

    deply production

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,1 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!******************************!*\
-  !*** ./resources/js/main.js ***!
-  \******************************/
-setTimeout(function () {
-  $(".loader").fadeToggle();
-  $(".main").fadeIn(1800);
-}, 1800);
-/******/ })()
-;
+setTimeout((function(){$(".loader").fadeToggle(),$(".main").fadeIn(1800)}),1800);
