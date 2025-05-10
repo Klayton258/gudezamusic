fc5a97ec99cd82becc49292262a2f16219ce6659
Klayton Massango <kmassango@transcom.co.mz>
2025-05-10 12:02:54 +0200

    show subscriber user

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -23,23 +23,23 @@ class AdminController extends Controller
         $subscribers = Subscribers::all()->count();
         $lead = Lead::all()->count();
 
-        $data =[
-            'totalClients'=>$clients,
-            'totalSubscribers'=>$subscribers,
-            'totalLeads'=>$lead,
+        $data = [
+            'totalClients' => $clients,
+            'totalSubscribers' => $subscribers,
+            'totalLeads' => $lead,
         ];
 
         $Recentclients = Client::whereRaw('Date(created_at) = CURDATE()');
         $Recentclients = Client::all();
 
-        return view('admin.home', ['user'=>Auth::guard('users')->user(),'data'=>$data,'NewClients'=>$Recentclients]);
+        return view('admin.home', ['user' => Auth::guard('users')->user(), 'data' => $data, 'NewClients' => $Recentclients]);
     }
 
     function login()
     {
 
         Log::channel('main')->info("regdsgfsd");
-        if(Auth::guard('users')->check()){
+        if (Auth::guard('users')->check()) {
 
             return redirect(route('dash'));
         }
@@ -51,32 +51,30 @@ class AdminController extends Controller
         try {
             $fieldType = filter_var($request->username, FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
 
-        if(Auth::guard('users')->attempt([$fieldType => $request->username, 'password' =>
-         $request->password])) {
-             $user = User::where($fieldType, $request->username)->first();
+            if (Auth::guard('users')->attempt([$fieldType => $request->username, 'password' =>
+            $request->password])) {
+                $user = User::where($fieldType, $request->username)->first();
 
-             $token = $user->createToken($request->device_name)->plainTextToken;
+                $token = $user->createToken($request->device_name)->plainTextToken;
 
-             session()->put('token',$token);
+                session()->put('token', $token);
 
-             Log::channel('main')->info("New seeesion: ". $user->id);
-             return redirect(route('dash'));
+                Log::channel('main')->info("New seeesion: " . $user->id);
+                return redirect(route('dash'));
             }
 
             Log::channel('main')->info("Wrong credentials");
-         return redirect(route('loginAdmin'))->withErrors(['error'=>'Login invalido, as suas credenciais sao incorretas!']);
-
-
-       } catch (Exception $e) {
-        $level = 'error';
-        $message = "BackOffice Error Login ";
-        $data = " Name: ".$request->name."- User: "
-        .$request->username." role- "
-        .$request->role_id.' >>>>> '
-        .$e->getMessage();
-        Log::channel('main')->$level($message." [".$data."]");
-        return back();
-       }
+            return redirect(route('loginAdmin'))->withErrors(['error' => 'Login invalido, as suas credenciais sao incorretas!']);
+        } catch (Exception $e) {
+            $level = 'error';
+            $message = "BackOffice Error Login ";
+            $data = " Name: " . $request->name . "- User: "
+                . $request->username . " role- "
+                . $request->role_id . ' >>>>> '
+                . $e->getMessage();
+            Log::channel('main')->$level($message . " [" . $data . "]");
+            return back();
+        }
     }
 
     function logout(Request $request)
@@ -93,116 +91,116 @@ class AdminController extends Controller
     function createUser()
     {
 
-        return view('admin.create',['user'=>Auth::guard('users')->user()]);
+        return view('admin.create', ['user' => Auth::guard('users')->user()]);
     }
 
     function newUser(Request $request)
     {
 
-            try {
+        try {
 
-                DB::table('users')->insert([
-                    'role_id'=> $request->role_id,
-                    'name'=> $request->name,
-                    'email'=> $request->email,
-                    'username'=> $request->username,
-                    'password'=> Hash::make($request->password),
-                    'created_at'=>now(),
-                    'updated_at'=>now(),
+            DB::table('users')->insert([
+                'role_id' => $request->role_id,
+                'name' => $request->name,
+                'email' => $request->email,
+                'username' => $request->username,
+                'password' => Hash::make($request->password),
+                'created_at' => now(),
+                'updated_at' => now(),
 
-                ]);
+            ]);
 
-                $from = ['email'=>env('MAIL_FROM_ADDRESS')];
+            $from = ['email' => env('MAIL_FROM_ADDRESS')];
 
-                $id = DB::table('users')->where(['email'=> $request->email])->first();
+            $id = DB::table('users')->where(['email' => $request->email])->first();
 
-                $data =['username'=>$request->username, 'password'=>$request->password, 'url'=>url('/verifyemail/'.$id->id)];
+            $data = ['username' => $request->username, 'password' => $request->password, 'url' => url('/verifyemail/' . $id->id)];
 
-                    Mail::send('mail.credentials', $data, function($m) use ($request,$from){
-                        $m->from('gudeza@gudezamusic.com', env('APP_NAME'));
-                        $m->to($request->email)->subject('Account Created');
-                    });
+            Mail::send('mail.credentials', $data, function ($m) use ($request, $from) {
+                $m->from('gudeza@gudezamusic.com', env('APP_NAME'));
+                $m->to($request->email)->subject('Account Created');
+            });
 
-                    $level = 'info';
-                    $message = "BackOffice User Created: ";
-                    $data = " Name: ".$request->name."- User: ".$request->username;
-                    Log::channel('main')->$level($message." [".$data."]");
+            $level = 'info';
+            $message = "BackOffice User Created: ";
+            $data = " Name: " . $request->name . "- User: " . $request->username;
+            Log::channel('main')->$level($message . " [" . $data . "]");
 
-                    return back()->with('success', 'Novo User Utilizador com sucesso.');
-
-
-           } catch (\Exception $e) {
-               if (config('app.debug')) {
+            return back()->with('success', 'Novo User Utilizador com sucesso.');
+        } catch (\Exception $e) {
+            if (config('app.debug')) {
                 $level = 'error';
-                    $message = "BackOffice Error Creating User: ";
-                    $data = " Name: ".$request->name."- User: "
-                    .$request->username." role- "
-                    .$request->role_id.' >>>>> '
-                    .$e->getMessage();
+                $message = "BackOffice Error Creating User: ";
+                $data = " Name: " . $request->name . "- User: "
+                    . $request->username . " role- "
+                    . $request->role_id . ' >>>>> '
+                    . $e->getMessage();
 
-                    Log::channel('main')->$level($message." [".$data."]");
-                    return back()->with('erro', 'Erro ao criar Utilizador.');
-               }
-                $level = 'error';
-                    $message = "BackOffice Error Creating User: ";
-                    $data = " Name: ".$request->name."- User: "
-                    .$request->username." role- "
-                    .$request->role_id.' >>>>> '
-                    .$e->getMessage();
+                Log::channel('main')->$level($message . " [" . $data . "]");
+                return back()->with('erro', 'Erro ao criar Utilizador.');
+            }
+            $level = 'error';
+            $message = "BackOffice Error Creating User: ";
+            $data = " Name: " . $request->name . "- User: "
+                . $request->username . " role- "
+                . $request->role_id . ' >>>>> '
+                . $e->getMessage();
 
-                    Log::channel('main')->$level($message." [".$data."]");
-                    return back()->with('erro', 'Erro ao criar Utilizador.');
-           }
-
+            Log::channel('main')->$level($message . " [" . $data . "]");
+            return back()->with('erro', 'Erro ao criar Utilizador.');
+        }
     }
 
     function uploadmusic()
     {
 
-        return view('admin.musics.uploadmusic',['user'=>Auth::guard('users')->user()]);
+        return view('admin.musics.uploadmusic', ['user' => Auth::guard('users')->user()]);
     }
 
     function updateabout()
     {
         $about = About::all()->first();
 
-        return view('admin.about.about', ['user'=>Auth::guard('users')->user(), 'about'=>$about]);
+        return view('admin.about.about', ['user' => Auth::guard('users')->user(), 'about' => $about]);
     }
 
     function refreshabout(Request $request)
     {
 
-       try {
+        try {
 
-        $about = About::all()->first();
+            $about = About::all()->first();
 
-        if($about != null) {
-        $about->title = $request->title;
-        $about->content = $request->content;
-        $about->save();
-        }else{
-        $about = DB::table('abouts')->insert([
-            'title'=> $request->title,
-            'content'=> $request->content,
-            'created_at'=>now(),
-            'updated_at'=>now(),
+            if ($about != null) {
+                $about->title = $request->title;
+                $about->content = $request->content;
+                $about->save();
+            } else {
+                $about = DB::table('abouts')->insert([
+                    'title' => $request->title,
+                    'content' => $request->content,
+                    'created_at' => now(),
+                    'updated_at' => now(),
 
-        ]);
+                ]);
+            }
+
+            return back()->with('success', 'Sobre nos atualizado com sucesso.', compact('about'));
+        } catch (Exception $e) {
+
+            return back()->with('erro', 'Erro ao atualizar sobre nos.');
         }
-
-        return back()->with('success', 'Sobre nos atualizado com sucesso.', compact('about'));
-
-       } catch (Exception $e) {
-
-        return back()->with('erro', 'Erro ao atualizar sobre nos.');
-       }
-
     }
 
     function subscribers()
     {
-        $subscribers = Subscribers::all();
-
-        return view('admin.subscribers.table', compact('subscribers'));
+        try {
+            $subscribers = Subscribers::all();
+            $user =  Auth::guard('users')->user();
+            return view('admin.subscribers.table', compact('subscribers', 'user'));
+        } catch (Exception $e) {
+            Log::error("ERROR SHOWING SUBSCRIBERS", ['exception' => $e->getMessage()]);
+            return back();
+        }
     }
 }
